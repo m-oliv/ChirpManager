e9206d094d846c43034063268cbf1531bd6f30ad
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-04-23 23:09:38 +0100

    - Added prototype of the retweet function.

--- bot.js
+++ bot.js
@@ -2,3 +2,12 @@ var twit = require('twit');
 var config = require('./config.js');
 
 var Twitter = new twit(config);
+
+var retweet = function(){
+    var params = {
+        q: '#freecodecamp, #TeslaMotors, #Tesla, #AstonMartin, #ISS, #space',
+        result_type: 'recent',
+        lang: 'en'
+    }
+}
+
